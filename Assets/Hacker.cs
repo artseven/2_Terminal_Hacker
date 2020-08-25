023d5d790016659b7dac88ee8bc0889b5dbe722a
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2020-08-25 11:29:16 -0400

    changes

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,54 +7,48 @@ public class Hacker : MonoBehaviour {
 
     // Game state
     int level;
-    enum Screen { MainMenu, Password, Win };
-    Screen currentScreen = Screen.MainMenu;
+    enum Screen { MainMenu, Password, Win }
+    Screen currentScreen;
 
-	// Use this for initialization
-	void Start ()
-    {
+    // Use this for initialization
+    void Start () {
         ShowMainMenu ();
     }
 
-    void ShowMainMenu ()
-    {
-        Terminal.ClearScreen();
-        Terminal.WriteLine("What would you like to hack into?");
-        Terminal.WriteLine("Press 1 for the local library");
-        Terminal.WriteLine("Press 2 for the police station");
-        Terminal.WriteLine("Enter your selection:");
+    void ShowMainMenu () {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen ();
+        Terminal.WriteLine ("What would you like to hack into?");
+        Terminal.WriteLine ("Press 1 for the local library");
+        Terminal.WriteLine ("Press 2 for the police station");
+        Terminal.WriteLine ("Enter your selection:");
     }
 
-    void OnUserInput(string input)
-    {
-        if (input == "menu")
-        {
-            ShowMainMenu();
-        } // TODO handle differently depending on screen
-        else if (input == "1")
-        {
+    //this should only decide how to handle user input
+    void OnUserInput (string input) {
+        if (input == "menu") {
+            ShowMainMenu ();
+        } else if (currentScreen == Screen.MainMenu) {
+            RunMainMenu (input);
+        } 
+    }
+    void RunMainMenu (string input) {
+        if (input == "1") {
             level = 1;
-            StartGame();
-        }
-        else if (input == "2")
-        {
+            StartGame ();
+        } else if (input == "2") {
             level = 2;
-            StartGame();
-        }
-        else if (input == "007")
-        {
-            Terminal.WriteLine("Please select a level Mr Bond!");
-        }
-        else
-        {
-            Terminal.WriteLine("Please choose a valid level");
+            StartGame ();
+        } else if (input == "007") {
+            Terminal.WriteLine ("Please select a level Mr Bond!");
+        } else {
+            Terminal.WriteLine ("Please choose a valid level");
         }
     }
 
-    void StartGame()
-    {
+    void StartGame () {
         currentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Please enter your password: ");
+        Terminal.WriteLine ("You have chosen level " + level);
+        Terminal.WriteLine ("Please enter your password: ");
     }
 }
